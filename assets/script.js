6fb35e6267f46fb991ea200e7b0ead34a9c51ce6
jam <jaymee602@gmail.com>
2022-05-06 12:11:59 -0700

    yesSpecial and noSpecial outside of function

--- assets/script.js
+++ assets/script.js
@@ -1,40 +1,85 @@
+
+// // Assignment code here
+// var generatePassword = function () {
+//   // Ask user how long they's like their password to be
+//   var getLength = window.prompt("How many characters would you like your password to contain?")
+//   if (!getLength || getLength < 8 || getLength > 128) {
+//     window.alert("Please enter a number between 8 and 128.")
+//     return generatePassword ();
+//   }
+//   if (getLength > 7 || getLength < 129) {
+//     console.log(getLength);
+//     var characterConfirm = window.confirm("Would you like your password to contain special characters?")
+//     if (characterConfirm === true) {
+//       console.log("yes special characters")
+//       var yesSpecial = function (length) {
+//         var result = '';
+//         var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!?+-()@#$%^&*:;,./'
+//         var charactersLength = characters.length;
+//         for ( var i=0; i<length; i++) {
+//           result += characters.charAt(Math.floor(Math.random() * charactersLength));
+//         } return result;
+//       }
+//       console.log(yesSpecial(getLength));
+//     } else if (characterConfirm === false) {
+//       console.log("no special characters")
+//       var noSpecial = function (length) {
+//         var result = '';
+//         var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890'
+//         var charactersLength = characters.length;
+//         for ( var i=0; i<length; i++) {
+//           result += characters.charAt(Math.floor(Math.random() * charactersLength));
+//         } return result;
+//       }
+//       console.log(noSpecial(getLength));
+//     } 
+    
+//   }
+// }
+
+
 // Assignment code here
+
+var noSpecial = function (length) {
+  var result = '';
+  var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890';
+  var charactersLength = characters.length;
+  for ( var i=0; i<length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  } return result;
+}
+
+var yesSpecial = function (length) {
+  var result = '';
+  var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!?+-()@#$%^&*:;,./';
+  var charactersLength = characters.length;
+  for ( var i=0; i<length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  } return result;
+}
+
 var generatePassword = function () {
   // Ask user how long they's like their password to be
-  var getLength = window.prompt("How many characters would you like your password to contain?")
+  var getLength = window.prompt("How many characters would you like your password to contain?");
   if (!getLength || getLength < 8 || getLength > 128) {
-    window.alert("Please enter a number between 8 and 128.")
+    window.alert("Please enter a number between 8 and 128.");
     return generatePassword ();
   }
   if (getLength > 7 || getLength < 129) {
     console.log(getLength);
-    var characterConfirm = window.confirm("Would you like your password to contain special characters?")
+    var characterConfirm = window.confirm("Would you like your password to contain special characters?");
     if (characterConfirm === true) {
-      console.log("yes special characters")
-      var yesSpecial = function (length) {
-        var result = '';
-        var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!?+-()@#$%^&*:;,./'
-        var charactersLength = characters.length;
-        for ( var i=0; i<length; i++) {
-          result += characters.charAt(Math.floor(Math.random() * charactersLength));
-        } return result;
-      }
+      console.log("yes special characters");
       console.log(yesSpecial(getLength));
+      yesSpecial(getLength);
     } else if (characterConfirm === false) {
-      console.log("no special characters")
-      var noSpecial = function (length) {
-        var result = '';
-        var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890'
-        var charactersLength = characters.length;
-        for ( var i=0; i<length; i++) {
-          result += characters.charAt(Math.floor(Math.random() * charactersLength));
-        } return result;
-      }
+      console.log("no special characters");
       console.log(noSpecial(getLength));
+      noSpecial(getLength);
     } 
-    
   }
 }
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -50,3 +95,6 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
